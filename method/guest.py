af6bcfe857a86012f82ce95e7ef9cc8278f36e1f
gizmore <gizmore@wechall.net>
2025-01-17 22:53:07 +0100

    'bootstrappy'

--- method/guest.py
+++ method/guest.py
@@ -3,7 +3,9 @@ from gdo.core.GDT_Name import GDT_Name
 from gdo.core.GDT_Password import GDT_Password
 from gdo.core.GDT_UserType import GDT_UserType
 from gdo.form.GDT_Form import GDT_Form
+from gdo.form.GDT_Submit import GDT_Submit
 from gdo.form.MethodForm import MethodForm
+from gdo.login.GDT_Login import GDT_Login
 from gdo.net.GDT_Url import GDT_Url
 from gdo.ui.GDT_Link import GDT_Link
 
@@ -18,11 +20,14 @@ class guest(MethodForm):
 
     def gdo_create_form(self, form: GDT_Form) -> None:
         form.text('md_register_guest')
-        form.add_field(GDT_Name('login').not_null())
+        form.add_field(GDT_Login('login').not_null())
         if module_enabled('login'):
             form.add_field(GDT_Password('password').not_null())
         form.add_field(GDT_Url('_back_to').internal().hidden())
         super().gdo_create_form(form)
+
+    def gdo_submit_button(self) -> GDT_Submit:
+        return super().gdo_submit_button().label('btn_as_guest')
 
     def form_submitted(self):
         username = self.param_val('login')
